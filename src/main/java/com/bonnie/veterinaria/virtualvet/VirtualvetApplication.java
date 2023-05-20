d70b0e209f2f1fa72b2f2d415016fa47887e119a
Andy Gomez <45979588+Koato@users.noreply.github.com>
2023-05-19 22:40:22 -0500

    first commit

---
+++ src/main/java/com/bonnie/veterinaria/virtualvet/VirtualvetApplication.java
@@ -0,0 +1,13 @@
+package com.bonnie.veterinaria.virtualvet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualvetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualvetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bonnie/veterinaria/virtualvet/VirtualvetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bonnie.veterinaria.virtualvet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualvetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
